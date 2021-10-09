413bf10d764bc39a67ae7b8dfe18f37ffe60bce0
diadolyy <niceline111@gmail.com>
2021-10-09 18:07:16 +0700

    remove test commands

--- app/js/search.js
+++ app/js/search.js
@@ -1,12 +1,9 @@
 document.getElementById('button').onclick= function(){
     let val=document.querySelector('input').value;
-    console.log(val);
     let items= document.querySelectorAll('#name');
-    console.log(items);
     let err=document.querySelector('.error');
     let counter=0;
     let valReg=new RegExp(val, "ig");
-    console.log(valReg);
 
     if(val!=''){
         items.forEach(function(elem){
@@ -21,8 +18,6 @@ document.getElementById('button').onclick= function(){
                 let str=elem.innerText;
                 elem.innerHTML=insertmark(str, elem.innerText.search(valReg),val.length);
             }
-            
-            console.log(err.innerText);
         })
 
         if (counter==0){
